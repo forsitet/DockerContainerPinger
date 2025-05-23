a7beb6226eee8e7fcdd43d61ddbff6aba461a431
Rekill <havelkaslana@gmail.com>
2025-05-24 00:21:37 +0300

    Updated tests

--- frontend/test/ widget_test.dart
+++ frontend/test/ widget_test.dart
@@ -1,12 +1,63 @@
+// ignore_for_file: file_names
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:exapmle_docker_pinger/generated/app_localizations.dart';
+import 'package:exapmle_docker_pinger/generated/app_localizations_en.dart';
+import 'package:exapmle_docker_pinger/generated/app_localizations_ru.dart';
+import 'package:exapmle_docker_pinger/src/features/authorization/presentation/pages/login_page.dart';
+import 'package:exapmle_docker_pinger/src/core/presentation/bloc/auth/auth_bloc.dart';
+import 'package:exapmle_docker_pinger/src/core/presentation/bloc/auth/auth_event.dart';
+import 'package:exapmle_docker_pinger/src/core/presentation/bloc/auth/auth_state.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthBloc extends Mock implements AuthBloc {}
+
+class FakeAuthEvent extends Fake implements AuthEvent {}
+
+class FakeAuthState extends Fake implements AuthState {}
 
 void main() {
-  test('String split', () {
-    var string = "foo bar";
-    expect(string.split(" "), equals(["foo", "bar"]));
+  setUpAll(() {
+    registerFallbackValue(FakeAuthEvent());
+    registerFallbackValue(FakeAuthState());
   });
-  test('Simple computation)', () {
-    final a = 1 + 1;
-    expect(a, 2);
+
+  group('LoginPage', () {
+    late AuthBloc authBloc;
+
+    setUp(() {
+      authBloc = MockAuthBloc();
+    });
+
+    Widget createWidgetUnderTest() {
+      return MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: BlocProvider<AuthBloc>.value(
+          value: authBloc,
+          child: const LoginPage(),
+        ),
+      );
+    }
+
+    testWidgets('shows error snackbar on AuthError', (tester) async {
+      whenListen(
+        authBloc,
+        Stream<AuthState>.fromIterable([
+          AuthError(error: AppLocalizationsRu().auth_failure),
+          AuthError(error: AppLocalizationsEn().auth_failure)
+        ]),
+        initialState: AuthInitial(),
+      );
+
+      await tester.pumpWidget(createWidgetUnderTest());
+      await tester.pump();
+      await tester.pump(const Duration(seconds: 1));
+
+      expect(find.byType(SnackBar), findsOneWidget);
+    });
   });
 }
